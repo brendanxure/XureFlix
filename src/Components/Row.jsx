175df30667693de51c05a306650303326e45f85c
brendanxure <lindaamadigwe@hotmail.com>
2023-03-28 15:35:37 +0200

    update

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -9,14 +9,14 @@ import { auth, db } from '../Firebase/Config'
 const Row = ({title, Content, id}) => {
     console.log(Content)
 
-    const [like, setLike] = useState(false)
+    const [like, setLike] = useState([])
     // const [save, setSave] = useState(false)
 
     const movieId = doc(db, 'userMovies', `${auth?.currentUser?.email}`)
 
-    const saveShow = async() => {
+    const saveShow = async(id) => {
         if(auth?.currentUser) {
-            setLike(!like)
+            setLike(id)
             // await addDoc(movieId, {
             //     savedShow: arrayUnion({
             //         id: item?.id,
@@ -44,12 +44,12 @@ const Row = ({title, Content, id}) => {
         <div className='flex items-center group/pep'>
             <MdChevronLeft onClick={slideleft} size={40} className='hidden group-hover/pep:block bg-white rounded-full left-2 absolute z-10 opacity-50 hover:opacity-100 cursor-pointer text-black'/>
         <div id={'slider' + id} className='flex whitespace-nowrap scroll-smooth relative overflow-x-scroll gap-4 scrollbar-hide'>
-            {Content?.map((item, id) =>
-            <div key={id} className='m-2 min-w-[300px] h-[200px] cursor-pointer relative'>
+            {Content?.map((item) =>
+            <div key={item?.id} className='m-2 min-w-[300px] h-[200px] cursor-pointer relative'>
                 <img className='w-full min-w-[300px] h-full object-cover' src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`} alt="/" />
                 <div className='group w-full h-full hover:bg-black/50 absolute left-0 top-0 mx-auto'>
                     <p className='group-hover:block hidden absolute top-[50%] items-center text-center h-full w-full text-lg'>{item.title}</p>
-                    <p>{like ? <AiFillHeart onClick={saveShow} className='relative top-4 left-4 group-hover:block hidden' /> : <AiOutlineHeart onClick={saveShow} className='relative top-4 left-4 group-hover:block hidden' />}</p>
+                    <p>{like == item.id ? <AiFillHeart onClick={()=>saveShow(item?.id)} className='relative top-4 left-4 group-hover:block hidden' /> : <AiOutlineHeart onClick={()=>saveShow(item.id)} className='relative top-4 left-4 group-hover:block hidden' />}</p>
                     {/* {like ? <div onClick={()=>saveShow(id)}><AiFillHeart /></div> : <div onClick={saveShow} ><AiOutlineHeart /></div> } */}
                 </div>
             </div>    
